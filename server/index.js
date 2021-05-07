ca142df0c5a5470b23fe861f058baf9b80c1f245
Anil Kumar <anilk1sagar@gmail.com>
2021-05-07 14:07:52 +0530

    Adding compression to express server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 import express from 'express';
 import Loadable from 'react-loadable';
-
 import indexController from './controllers/index';
+
+const compression = require('compression');
 
 const PORT = 3000;
 
 // initialize the application and create the routes
 const app = express();
+
+const shouldCompress = (req, res) => {
+    if (req.headers['x-no-compression']) {
+        // Will not compress responses, if this header is present
+        return false;
+    }
+    // Resort to standard compression
+    return compression.filter(req, res);
+};
+
+// Compress all HTTP responses
+app.use(compression({
+    filter: shouldCompress,
+    threshold: 0
+}));
 
 app.use(indexController);
 
