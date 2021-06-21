f25bb28bef879e2d960160add1b7d9c2e3a0c7be
Anil Kumar <anilk1sagar@gmail.com>
2021-06-21 14:30:06 +0530

    Update bootstrap.js

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -5,7 +5,7 @@ require("file-loader");
 
 // Babel Configs
 require("@babel/register")({
-  ignore: [/(node_modules)/, ".css"],
+  ignore: [/(node_modules)/],
   presets: ["@babel/preset-env", "@babel/preset-react"],
   plugins: [
     "syntax-dynamic-import",
